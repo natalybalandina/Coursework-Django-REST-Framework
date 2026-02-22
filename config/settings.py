58a66a24cdbb085b026b17f7375615d7dcfad25e
bal1nataly <bal1nataly@gmail.com>
2026-02-22 23:41:43 +0000

    Add STATIC_ROOT and MEDIA_ROOT settings

--- config/settings.py
+++ config/settings.py
@@ -155,6 +155,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+MEDIA_URL = "media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
